be9c2fd006341d72bd4a3b84a140b58650bb7416
sandip22sandip <108937509+sandip22sandip@users.noreply.github.com>
2022-07-13 11:49:57 +0530

    Update lib.user.js
    Updated Delete Function Lib Folder.

--- app/lib/lib.user.js
+++ app/lib/lib.user.js
@@ -4,6 +4,19 @@ const bcrypt                = require('bcrypt');
 let dbConn                  = require('../../database.js');
 
 module.exports = {
+    
+    getAllUsers: () => {
+        return new Promise((resolve, reject) => {
+            dbConn.query(`SELECT * FROM ?? WHERE idst != ?`, ["core_user", "270"], (err, results) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                } 
+            });
+        });
+    },
+
     checkUseridExists: (userid) => {
         return new Promise((resolve, reject) => {
             dbConn.query( `SELECT * FROM core_user WHERE userid = '${userid}'`, (err, results) => {
@@ -31,7 +44,6 @@ module.exports = {
     registerUser: (userid, firstname, lastname, email, pass, user_type) => {
         return new Promise((resolve, reject) => {
             let qIUser = `INSERT INTO core_user (userid, firstname, lastname, email, pass, user_type) VALUES ('${userid}', '${firstname}', '${lastname}', '${email}', '${pass}', '${user_type}')`;
-
             dbConn.query(qIUser, (err, results) => {
                 if(err){
                     reject(err);
@@ -46,6 +58,19 @@ module.exports = {
         return new Promise((resolve, reject) => {
             let qUpdateUser = `UPDATE core_user SET userid = '${userid}', firstname = '${firstname}', lastname = '${lastname}', email = '${email}', pass = '${pass}', user_type ='${user_type}' WHERE idst = '${idst}'`;
             dbConn.query(qUpdateUser, (err, results) => {
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(results);
+                }
+            });
+        });
+    },
+
+    deleteUser: (idst) => {
+        return new Promise((resolve, reject) => {
+            let qDelUser = `DELETE FROM core_user WHERE idst = '${idst}'`;
+            dbConn.query(qDelUser, (err, results) => {
                 if(err){
                     reject(err);
                 }else{
